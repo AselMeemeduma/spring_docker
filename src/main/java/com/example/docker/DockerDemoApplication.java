35d0488fefa51de1280333303bd849772e9a4865
Chamith Kodikara <chamith@ceylonit.com>
2018-03-28 11:32:36 +0530

    -initial commit

---
+++ src/main/java/com/example/docker/DockerDemoApplication.java
@@ -0,0 +1,20 @@
+package com.example.docker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DockerDemoApplication {
+
+	@GetMapping("/")
+	public String home(){
+		return "WELCOME..! DOCKER DEMO";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerDemoApplication.class, args);
+	}
+}
